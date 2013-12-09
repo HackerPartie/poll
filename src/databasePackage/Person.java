a096c9421c2af393efc6bfdcf2f25589b7461637
Emmanuel Kasper <emmanuel@libera.cc>
2013-12-10 00:06:41 +0100

    first test in mysql

--- src/databasePackage/Person.java
+++ src/databasePackage/Person.java
@@ -1,17 +1,53 @@
 package databasePackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Person {
+	
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement =null;
+	private ResultSet resultSet = null;
+	
+	public void connectDB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.
+					getConnection("jdbc:mysql://localhost/poll", "polluser", "pollpassword");
+			// Statements allow to issueSQL queries to the Database
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM person");
+			writeResultSet(resultSet);
+		} 
+		catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+	}
+	
+	public void writeResultSet(ResultSet resultSet) throws SQLException {
+		while (resultSet.next()) {
+			String name = resultSet.getString("NAME");
+			String qsum = resultSet.getString("qsum");
+			System.out.println("Person Name: " + name);
+			System.out.println("Person Score: " + qsum);
+			
+			
+		}
+	}
 
 	public void savePersonScore(
 			String personName,
-			int q1, int q2, int q3,
-			int q4, int q5, int q6,
-			int q7, int q8, int q9,
-			int Sum) {
+			int[] personAnswers,
+			int personSum) {
 		// put the stuff in the database
 		// aka INSERT INTO person VALUES (default, 'lars', 1 , 2, 3, 1, 2, 3, 1, 2, 3, 1, 18);
 		String queryString = "INSERT INTO person VALUES (default, personName, q1, q2, q3, q4, q5, q6, q7, q8, q9, Sum)";
-		System.out.println(queryString);
 	}
 	
 	public double getAverage(){
