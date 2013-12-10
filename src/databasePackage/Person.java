46d92269e005853136bca129e8408628b040c0a8
Emmanuel Kasper <emmanuel@libera.cc>
2013-12-10 21:16:13 +0100

    implement database layer with mysql

--- src/databasePackage/Person.java
+++ src/databasePackage/Person.java
@@ -7,51 +7,139 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * @author manu
+ * 
+ */
+
 public class Person {
-	
+
 	private Connection connect = null;
 	private Statement statement = null;
-	private PreparedStatement preparedStatement =null;
+	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
 	
-	public void connectDB() {
+	// count the number of saved persons,
+	//  ie count number of row in the person TABLE
+	private int numRows;
+	private int[] allScores;
+	double sumAllScores;
+	
+	/*
+	 * put the stuff in the database aka INSERT INTO person VALUES (default,
+	 * 'lars', 1 , 2, 3, 1, 2, 3, 1, 2, 3, 18);
+	 */
+	public void savePersonScore(String personName, int[] personAnswers,
+			int personSum) {
+
+		try {
+			// load the jdbc driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// establish a TCP/IP connection with mysql
+			connect = DriverManager.getConnection(
+					"jdbc:mysql://localhost/poll", "polluser", "pollpassword");
+
+			// create a preparedStatement object, which is going to hold our SQL
+			// statement
+			preparedStatement = connect
+					.prepareStatement("INSERT INTO person VALUES(default, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+			// fill the SQL query with the paramaters we want to save,
+			// starting with the personName in the first ? placeholder from
+			// above
+			preparedStatement.setString(1, personName);
+
+			// this would boring
+			// preparedStatement.setInt(2, personAnswers[0]);
+			// preparedStatement.setInt(3, personAnswers[1]);
+			// so we do it a loop
+			for (int i = 0; i < personAnswers.length; i++) {
+				preparedStatement.setInt(i + 2, personAnswers[i]);
+			}
+
+			// add the score as the last parameter
+			preparedStatement.setInt(11, personSum);
+
+			// execute the SQL statement
+			preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+	}
+	
+	// get the average of all saved scores
+	public double getAverage() {
+		try {
+			
+			// load the jdbc driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// establish a TCP/IP connection with mysql
+			connect = DriverManager.getConnection(
+					"jdbc:mysql://localhost/poll", "polluser", "pollpassword");
+
+			// create a preparedStatement object, which is going to hold our SQL statement			
+			// count the number of saved persons,
+			//  ie count number of row in the person TABLE
+			preparedStatement = connect
+					.prepareStatement("SELECT COUNT(*) FROM person");
+			resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				numRows = resultSet.getInt("count(*)");
+			}
+			
+			// now we get to retrieve the score of all persons in the table
+			// to save the scores we initialize an int array of the size of the table person itself
+			allScores = new int[numRows];
+			preparedStatement = connect.prepareStatement("SELECT qsum FROM person;");
+			resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				allScores[resultSet.getRow()-1] = resultSet.getInt("qsum");	
+			}
+			
+			// print the content of our array
+			for (int i = 0; i < allScores.length; i++) {
+				sumAllScores += allScores[i];
+			}
+						
+			return sumAllScores / allScores.length;
+									
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return -1;
+		}
+	}
+	
+	/*
+	 *  a debug method, available only the inside package scope 
+	 */
+	 void displayAll() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connect = DriverManager.
-					getConnection("jdbc:mysql://localhost/poll", "polluser", "pollpassword");
-			// Statements allow to issueSQL queries to the Database
+			connect = DriverManager.getConnection(
+					"jdbc:mysql://localhost/poll", "polluser", "pollpassword");
 			statement = connect.createStatement();
 			resultSet = statement.executeQuery("SELECT * FROM person");
 			writeResultSet(resultSet);
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
-		
 	}
 	
-	public void writeResultSet(ResultSet resultSet) throws SQLException {
+	private void writeResultSet(ResultSet resultSet) throws SQLException {
 		while (resultSet.next()) {
 			String name = resultSet.getString("NAME");
 			String qsum = resultSet.getString("qsum");
 			System.out.println("Person Name: " + name);
 			System.out.println("Person Score: " + qsum);
-			
-			
+
 		}
 	}
 
-	public void savePersonScore(
-			String personName,
-			int[] personAnswers,
-			int personSum) {
-		// put the stuff in the database
-		// aka INSERT INTO person VALUES (default, 'lars', 1 , 2, 3, 1, 2, 3, 1, 2, 3, 1, 18);
-		String queryString = "INSERT INTO person VALUES (default, personName, q1, q2, q3, q4, q5, q6, q7, q8, q9, Sum)";
-	}
-	
-	public double getAverage(){
-		return 25.0;
-	}
-	
 }
--- src/databasePackage/TestPerson.java
+++ src/databasePackage/TestPerson.java
@@ -7,8 +7,23 @@ public class TestPerson {
 	 */
 	public static void main(String[] args) {
 		
+
+		// try to write Ulrich to the database
+		String ulrichName = "Ulrich";
+		int[] ulrichAnswers = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+		int ulrichScore = 18;
+		
+		new Person().savePersonScore(ulrichName, ulrichAnswers, ulrichScore);
+//		
+//		ulrichScore = 25;
+//		new Person().savePersonScore(ulrichName, ulrichAnswers, ulrichScore);
+//		
+//		ulrichScore = 26;
+//		new Person().savePersonScore(ulrichName, ulrichAnswers, ulrichScore);
+		
 		System.out.println(new Person().getAverage());
-		new Person().connectDB();
+		System.exit(0);
+		
 				
 		// new Person().savePersonScore("manu", 1, 2, 3, 3, 3, 3, 3, 1, 3, 25);
 		
