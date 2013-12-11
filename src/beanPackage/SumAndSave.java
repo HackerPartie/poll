8e9a565af042235ead642dcb780a2319d5cfc01a
Bergsocke <bergsocke@gmx.at>
2013-12-11 19:52:38 +0100

    Auslagern save-Funktion, Datensaetze werden nur gespeichert, wenn alle Fragen beantwortet wurden

--- src/beanPackage/SumAndSave.java
+++ src/beanPackage/SumAndSave.java
@@ -2,16 +2,20 @@ package beanPackage;
 
 import javax.servlet.http.HttpServletRequest;
 
+import databasePackage.Person;
 
-public class QuestionSum {
+
+public class SumAndSave {
 	
 	private int sumQuestions;
 	private int[] wrongAnswers ={0,0,0,0,0,0,0,0,0};
 	private int[] myIntAnswers = new int[9];
+	private String txtWrongAnswers ="";
+
 	
-	public void addition(HttpServletRequest myRequest) {
+	public void additionAndSave(HttpServletRequest myRequest) {
 		
-		// Uebernahme der Antworten
+		// Uebernahme der Antworten 
 		String[] myArrayAnswers = new String[9];
 		myArrayAnswers[0] = myRequest.getParameter("question_1");
 		myArrayAnswers[1] = myRequest.getParameter("question_2");
@@ -22,6 +26,7 @@ public class QuestionSum {
 		myArrayAnswers[6] = myRequest.getParameter("question_7");
 		myArrayAnswers[7] = myRequest.getParameter("question_8");
 		myArrayAnswers[8] = myRequest.getParameter("question_9");
+		
 
 		for (int i = 0; i < myArrayAnswers.length; i++) {
 			try {
@@ -29,32 +34,41 @@ public class QuestionSum {
 				myIntAnswers[i]= Integer.parseInt(myArrayAnswers[i]);
 				sumQuestions += myIntAnswers[i];
 				
-			//falls eine Frage nicht beantwortet wurde, wird die Zahl der Frage gespeichert
+			//falls eine Frage nicht beantwortet wurde -> Abspeichern des IndexPlatzes
+			//und der Wert wird auf 0 gesetzt
 			} catch (Exception error) {
 				wrongAnswers[i] = i+1;
+				myIntAnswers[i] = 0;
 			}
 
 		}
+		
+		//Zusammenfassen der nicht-geklickten Antwortnummern in einen String für den Ausgabetext				
+		for (int i : wrongAnswers) {
+			if (i == 0) {
+				//do nothing
+			} else{
+				txtWrongAnswers += i + ", ";
+			}
+		}
+		
+		//User-Name uebernehmen
+		String userName = myRequest.getParameter("txtUsername");
+		
+		//Sicherstellen, dass Datensaetze nur gespeichert werden, wenn alle Fragen beantwortet wurden
+		if (txtWrongAnswers.equals("")) {
+			new Person().savePersonScore(userName, myIntAnswers, sumQuestions);
+		}
 	}
-
-	public int[] getWrongAnswers() {
-		return wrongAnswers;
-	}
-
-
-
+	
+		
+	//Getter	
 	public int getSumQuestions() {
 		return sumQuestions;
 	}
 
-	public int[] getMyIntAnswers() {
-		return myIntAnswers;
-	}
-	
-	
-	
-	
-	
-	
+	public String getTxtWrongAnswers() {
+		return txtWrongAnswers;
+	}	
 
 }
--- src/servletPackage/Controller.java
+++ src/servletPackage/Controller.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import databasePackage.Person;
 
-import beanPackage.QuestionSum;
+import beanPackage.SumAndSave;
 import beanPackage.TextResult;
 
 /**
@@ -26,38 +26,19 @@ public class Controller extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest myRequest, HttpServletResponse myResponse) throws ServletException, IOException {
 	
-		QuestionSum myQuestionSum = new QuestionSum();
+		SumAndSave mySumAndSave = new SumAndSave();
 		
-		//Ermittlung des User-Namens
-		String userName = myRequest.getParameter("txtUsername");
+		//Punkteanzahl ermitteln und Datensatz abspeichern
+		mySumAndSave.additionAndSave(myRequest);
+		int mySumQuestions = mySumAndSave.getSumQuestions();
 		
-		//Ermittlung des Ergebnisses
-		myQuestionSum.addition(myRequest);
-		int mySumQuestions = myQuestionSum.getSumQuestions();
-		
-		//Ermittlung der Integer-Werte der Antworten		
-		int[] tempArray = myQuestionSum.getMyIntAnswers();
-		//Array-Kopie erstellen
-		int[] intQ = new int[tempArray.length];
-		System.arraycopy(tempArray, 0, intQ, 0, intQ.length);
-
-		//Datensatz abspeichern
-		Person myPerson = new Person();
-		myPerson.savePersonScore(userName, intQ, mySumQuestions);
-	    
-	    //Fehlende Antworten ermitteln
-		int[] pWrong = myQuestionSum.getWrongAnswers();
-    
-	    //Ausgabetext ermitteln
-		String outputResult = new TextResult().outputText(mySumQuestions, pWrong);
-		
-		//Average ermitteln
-		double myAverage = new Person().getAverage();
+	    //Ausgabetext holen
+		String outputResult = new TextResult().outputText(mySumQuestions, mySumAndSave.getTxtWrongAnswers());
 	
 	    //Weiterleitung des Ergebnisses, des Textes und dem Durchschnitt an result.jsp
 		myRequest.setAttribute("sum", mySumQuestions);
 		myRequest.setAttribute("outputResult", outputResult );
-		myRequest.setAttribute("average", myAverage);
+		myRequest.setAttribute("average", new Person().getAverage());
 		RequestDispatcher rdp = myRequest.getRequestDispatcher("result.jsp");
 		rdp.forward(myRequest, myResponse);
 				
